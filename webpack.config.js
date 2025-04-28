c3d371406dab388e978119de6cfec096dfa6e4c4
Jallaldin Hamdard <jalladinhamdard@gmail.com>
2025-04-28 15:49:15 +0100

    add loaders for imported images and styling

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     mode: "development",
-    entry: "./src/index.js",
+    entry: "./src/reload.js",
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist"),
@@ -18,4 +18,21 @@ module.exports = {
             template: "./src/template.html",
         }),
     ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+
+            ]
+    }
 }
